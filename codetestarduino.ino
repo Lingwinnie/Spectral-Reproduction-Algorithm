b7cd5874af3d32b20eded67956d455f13128fd01
Lingwinnie <erwann.espiau@gmail.com>
2025-10-18 15:11:05 +0200

    Add files via upload

---
+++ codetestarduino.ino
@@ -0,0 +1,39 @@
+// Définition des broches
+const int ledPins[6] = {3, 5, 6, 9, 10, 11};
+int brightness[6] = {0, 0, 0, 0, 0, 0};
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < 6; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    analogWrite(ledPins[i], brightness[i]);
+  }
+}
+
+void loop() {
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    input.trim(); // Supprimer espaces/retours à la ligne
+
+    // Exemple de commande : L1:128
+    if (input.startsWith("L") && input.indexOf(':') > 1) {
+      int ledIndex = input.substring(1, input.indexOf(':')).toInt();
+      int value = input.substring(input.indexOf(':') + 1).toInt();
+
+      if (ledIndex >= 1 && ledIndex <= 6) {
+        value = constrain(value, 0, 255);
+        brightness[ledIndex - 1] = value;
+        analogWrite(ledPins[ledIndex - 1], value);
+
+        Serial.print("LED ");
+        Serial.print(ledIndex);
+        Serial.print(" -> ");
+        Serial.println(value);
+      } else {
+        Serial.println("Erreur : index LED invalide (1 à 6).");
+      }
+    } else {
+      Serial.println("Commande invalide. Format attendu : L1:128");
+    }
+  }
+}
